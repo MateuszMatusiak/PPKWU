b7c4b57962316a340b2d2651b3a763464344c83c
Mateusz Matusiak <krab879@gmail.com>
2022-12-22 13:09:18 +0100

    Przetworzenie

--- lab6/source/Server.java
+++ lab6/source/Server.java
@@ -2,11 +2,18 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 import org.xml.sax.SAXException;
 
+import javax.xml.bind.Marshaller;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathExpression;
 import javax.xml.xpath.XPathExpressionException;
@@ -42,13 +49,19 @@ public class Server {
 				in.append(charBuffer, 0, numCharsRead);
 			}
 
+			Data data;
+
 			InputStream stream = new ByteArrayInputStream(in.toString().getBytes(StandardCharsets.UTF_8));
 			try {
-				handleRequest(stream);
+				data = handleRequest(stream);
 			} catch (XPathExpressionException | ParserConfigurationException | SAXException e) {
 				throw new RuntimeException(e);
 			}
 
+			MathResult m = new MathResult(data.num1, data.num2);
+			Statistics s = calculateString(data.str);
+
+			
 			stream.close();
 //			t.sendResponseHeaders(200, response.toString().length());
 //			OutputStream os = t.getResponseBody();
@@ -56,66 +69,129 @@ public class Server {
 //			os.close();
 
 		}
-	}
+		public Data handleRequest(InputStream stream) throws XPathExpressionException, ParserConfigurationException, IOException, SAXException {
+			DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+			documentBuilderFactory.setNamespaceAware(true);
+			DocumentBuilder builder = documentBuilderFactory.newDocumentBuilder();
+			Document doc = builder.parse(stream); //input stream of response.
 
-	public static void handleRequest(InputStream stream) throws XPathExpressionException, ParserConfigurationException, IOException, SAXException {
-		DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
-		documentBuilderFactory.setNamespaceAware(true);
-		DocumentBuilder builder = documentBuilderFactory.newDocumentBuilder();
-		Document doc = builder.parse(stream); //input stream of response.
+			XPathFactory xPathFactory = XPathFactory.newInstance();
+			XPath xpath = xPathFactory.newXPath();
 
-		XPathFactory xPathFactory = XPathFactory.newInstance();
-		XPath xpath = xPathFactory.newXPath();
+			XPathExpression expr = xpath.compile("//str"); // Look for status tag value.
+			String str = expr.evaluate(doc);
 
-		XPathExpression expr = xpath.compile("//str"); // Look for status tag value.
-		String str =  expr.evaluate(doc);
+			XPathExpression expr1 = xpath.compile("//num1"); // Look for status tag value.
+			int num1 = Integer.parseInt(expr1.evaluate(doc));
 
-		XPathExpression expr1 = xpath.compile("//num1"); // Look for status tag value.
-		int num1 = Integer.parseInt(expr1.evaluate(doc));
+			XPathExpression expr2 = xpath.compile("//num2"); // Look for status tag value.
+			int num2 = Integer.parseInt(expr2.evaluate(doc));
 
-		XPathExpression expr2 = xpath.compile("//num2"); // Look for status tag value.
-		int num2 = Integer.parseInt(expr2.evaluate(doc));
+			return new Data(num1, num2, str);
 
-		System.out.println(str + num1 + num2);
-	}
-
-	class Statistics {
-		private final int lowercase;
-		private final int uppercase;
-		private final int digits;
-		private final int special;
-
-		public Statistics(int lowercase, int uppercase, int digits, int special) {
-			this.lowercase = lowercase;
-			this.uppercase = uppercase;
-			this.digits = digits;
-			this.special = special;
 		}
 
-	}
+		private static void writeXml(OutputStream output, MathResult math)
+				throws TransformerException, ParserConfigurationException {
 
-	class MathResult {
-		private int sum;
-		private int sub;
-		private int mul;
-		private int div;
-		private int mod;
+			DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+			Document doc = docBuilder.newDocument();
 
-		int num1;
-		int num2;
+			Element rootElement = doc.createElement("root");
+			doc.appendChild(rootElement);
 
-		public MathResult(int num1, int num2) {
-			this.num1 = num1;
-			this.num2 = num2;
-			calculate();
+			doc.createElement("num1+num2");
+			rootElement.appendChild(doc.createElement(String.valueOf(math.sum)));
+			doc.createElement("num1-num2");
+			rootElement.appendChild(doc.createElement(String.valueOf(math.sub)));
+			doc.createElement("num1*num2");
+			rootElement.appendChild(doc.createElement(String.valueOf(math.mul)));
+			doc.createElement("num1/num2");
+			rootElement.appendChild(doc.createElement(String.valueOf(math.div)));
+			doc.createElement("num1%num2");
+			rootElement.appendChild(doc.createElement(String.valueOf(math.mod)));
+
+			TransformerFactory transformerFactory = TransformerFactory.newInstance();
+			Transformer transformer = transformerFactory.newTransformer();
+			DOMSource source = new DOMSource(doc);
+			StreamResult result = new StreamResult(output);
+
+			transformer.transform(source, result);
+
 		}
 
-		private void calculate() {
-			sum = num1 + num2;
-			sub = num1 - num2;
-			mul = num1 * num2;
-			div = num1 / num2;
-			mod = num1 % num2;
+
+		public Statistics calculateString(String text) {
+			int lowercase = 0;
+			int uppercase = 0;
+			int digits = 0;
+			int special = 0;
+
+			char[] arr = text.toCharArray();
+			for (char c : arr) {
+				if (Character.isLowerCase(c)) {
+					lowercase++;
+				} else if (Character.isUpperCase(c)) {
+					uppercase++;
+				} else if (Character.isDigit(c)) {
+					digits++;
+				} else {
+					special++;
+				}
+			}
+			return new Statistics(lowercase, uppercase, digits, special);
+		}
+
+		class Statistics {
+			private final int lowercase;
+			private final int uppercase;
+			private final int digits;
+			private final int special;
+
+			public Statistics(int lowercase, int uppercase, int digits, int special) {
+				this.lowercase = lowercase;
+				this.uppercase = uppercase;
+				this.digits = digits;
+				this.special = special;
+			}
+
+		}
+
+		class MathResult {
+			private int sum;
+			private int sub;
+			private int mul;
+			private int div;
+			private int mod;
+
+			int num1;
+			int num2;
+
+			public MathResult(int num1, int num2) {
+				this.num1 = num1;
+				this.num2 = num2;
+				calculate();
+			}
+
+			private void calculate() {
+				sum = num1 + num2;
+				sub = num1 - num2;
+				mul = num1 * num2;
+				div = num1 / num2;
+				mod = num1 % num2;
+			}
+		}
+		class Data{
+			int num1;
+			int num2;
+			String str;
+
+			public Data(int num1, int num2, String str) {
+				this.num1 = num1;
+				this.num2 = num2;
+				this.str = str;
+			}
 		}
 	}
 }
